cf616bb07ad99a669d0af219cfbc93b1620d8e17
sammyon646 <sammyon646@gmail.com>
2025-12-10 18:41:15 +0300

    123

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 // если оставляешь d.ts с типами Telegram, можешь просто импортнуть его для TS:
-import './types/telegram' // либо './global' — в зависимости от того, как назвал файл с декларацией
+import './types/telegram.d.ts' // либо './global' — в зависимости от того, как назвал файл с декларацией
 
 if (window.Telegram?.WebApp) {
   window.Telegram.WebApp.ready()
