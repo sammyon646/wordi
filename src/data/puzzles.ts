2b86ddc6941ae64c8a3e9ecb0a1017a66c08dc3a
sammyon646 <sammyon646@gmail.com>
2025-12-11 17:37:09 +0300

    123

--- src/data/puzzles.ts
+++ src/data/puzzles.ts
@@ -13,41 +13,41 @@ export interface Puzzle {
 }
 
 export const puzzles: Puzzle[] = [
-  {
-    across: {
-      '1': { clue: 'Кот (англ.)', answer: 'CAT', row: 0, col: 0 },
-    },
-    down: {
-      '2': { clue: 'Действовать (англ.)', answer: 'ACT', row: 0, col: 1 },
-    },
-  },
-  {
-    across: {
-      '1': { clue: 'Десять (англ.)', answer: 'TEN', row: 1, col: 0 },
-    },
-    down: {
-      '2': { clue: 'Сеть (англ.)', answer: 'NET', row: 0, col: 1 },
-    },
-  },
-  {
-    across: {
-      '1': { clue: 'Выиграл (прош. время от win)', answer: 'WON', row: 0, col: 0 },
-      '3': { clue: 'Сейчас (англ.)', answer: 'NOW', row: 2, col: 1 },
-    },
-    down: {
-      '2': { clue: 'Владеть (англ.)', answer: 'OWN', row: 0, col: 1 },
-    },
-  },
-  {
-    across: {
-      '1': { clue: 'Стена', answer: 'WALL', row: 2, col: 0 },
-    },
-    down: {
-      '2': { clue: 'Закон (англ.)', answer: 'LAW', row: 0, col: 0 },
-      '3': { clue: 'Все (англ.)', answer: 'ALL', row: 0, col: 2 },
-    },
-  },
-  
+  // {
+  //   across: {
+  //     '1': { clue: 'Кот (англ.)', answer: 'CAT', row: 0, col: 0 },
+  //   },
+  //   down: {
+  //     '2': { clue: 'Действовать (англ.)', answer: 'ACT', row: 0, col: 1 },
+  //   },
+  // },
+  // {
+  //   across: {
+  //     '1': { clue: 'Десять (англ.)', answer: 'TEN', row: 1, col: 0 },
+  //   },
+  //   down: {
+  //     '2': { clue: 'Сеть (англ.)', answer: 'NET', row: 0, col: 1 },
+  //   },
+  // },
+  // {
+  //   across: {
+  //     '1': { clue: 'Выиграл (прош. время от win)', answer: 'WON', row: 0, col: 0 },
+  //     '3': { clue: 'Сейчас (англ.)', answer: 'NOW', row: 2, col: 1 },
+  //   },
+  //   down: {
+  //     '2': { clue: 'Владеть (англ.)', answer: 'OWN', row: 0, col: 1 },
+  //   },
+  // },
+  // {
+  //   across: {
+  //     '1': { clue: 'Стена', answer: 'WALL', row: 2, col: 0 },
+  //   },
+  //   down: {
+  //     '2': { clue: 'Закон (англ.)', answer: 'LAW', row: 0, col: 0 },
+  //     '3': { clue: 'Все (англ.)', answer: 'ALL', row: 0, col: 2 },
+  //   },
+  // },
+
   {
     across: {
       '1': { clue: 'Да (англ.)', answer: 'YES', row: 0, col: 0 },
